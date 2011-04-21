e9a5002d8f3ba507378124f3cf420fe68a8d03c9
cmar <christopher.mar@gmail.com>
2011-04-21 10:50:08 -0400

    Initial Commit

---
+++ FootballTeamRosterTests/FootballTeamRosterTests.h
@@ -0,0 +1,17 @@
+//
+//  FootballTeamRosterTests.h
+//  FootballTeamRosterTests
+//
+//  Created by Christopher Mar on 4/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface FootballTeamRosterTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ FootballTeamRosterTests/FootballTeamRosterTests.m
@@ -0,0 +1,33 @@
+//
+//  FootballTeamRosterTests.m
+//  FootballTeamRosterTests
+//
+//  Created by Christopher Mar on 4/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FootballTeamRosterTests.h"
+
+
+@implementation FootballTeamRosterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FootballTeamRosterTests");
+}
+
+@end
